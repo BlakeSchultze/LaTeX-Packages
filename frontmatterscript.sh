083dd17f02160e01f091cba9ddce6e0d85b43f73
Blake Schultze <Blake_Schultze@baylor.edu>
2016-07-12 04:24:54 -0500

    Initial commit of existing LaTeX package files

---
+++ frontmatterscript.sh
@@ -0,0 +1,2 @@
+makeglossaries -s my-glossary.ist -t pCT_Data_Organization_Format.glg  -o pCT_Data_Organization_Format.gls  pCT_Data_Organization_Format.glo
+makeindex -s my-acronyms.ist -t pCT_Data_Organization_Format.alg  -o pCT_Data_Organization_Format.acr  pCT_Data_Organization_Format.acn
