4fd90518192cb361b07d7cdf5f96e6709d770a2c
Blake Schultze <Blake_Schultze@baylor.edu>
2016-08-07 08:11:07 -0500

    Finished front/back matter config and package options/dependencies, separated/organized defs/commands, & began documentation
    (1) Finished front/back matter configurations/commands, e.g.:
    - Table of Contents sections/entries for List of Figures/Tables/Code
    Listings/Problemsconfigured via tocloft package
    - glossary/acronyms, abbreviations, index configured via adapted
    versions of the glossaries, nomencl, and imakeidx packages,
    respectively.
    (2) Register variable instantiations and \def definitions moved to
    separate package and math/font commands/environments moved to their
    corresponding packages
    (3) Color set definitions moved to separate package with each set
    defined in separate .def files
    (4) Old and/or unnecessary definitions/commands/etc removed from
    packages and each file then cleaned up and organized
    (5) Documentation on packages created, including color tables, and
    several .tex and .notes documents created with the comments and notes
    written inside package files moved to these new documentation files as
    part of the package cleanup process

--- frontmatterscript.sh
+++ frontmatterscript.sh
@@ -1,2 +1,7 @@
-makeglossaries -s my-glossary.ist -t pCT_Data_Organization_Format.glg  -o pCT_Data_Organization_Format.gls  pCT_Data_Organization_Format.glo
+makeindex -s my-nomencl.ist -t pCT_Data_Organization_Format.nlg  -o pCT_Data_Organization_Format.nls  pCT_Data_Organization_Format.nlo
 makeindex -s my-acronyms.ist -t pCT_Data_Organization_Format.alg  -o pCT_Data_Organization_Format.acr  pCT_Data_Organization_Format.acn
+makeglossaries -s my-glossary.ist -t pCT_Data_Organization_Format.glg  -o pCT_Data_Organization_Format.gls  pCT_Data_Organization_Format.glo
+makeindex -s my-index.mst -t pCT_Data_Organization_Format.ilg  -o pCT_Data_Organization_Format.ind  pCT_Data_Organization_Format.idx
+
+#makeglossaries -s my-glossary.ist -t pCT_Data_Organization_Format.glg  -o pCT_Data_Organization_Format.gls  pCT_Data_Organization_Format.glo
+#makeindex -s my-acronyms.ist -t pCT_Data_Organization_Format.alg  -o pCT_Data_Organization_Format.acr  pCT_Data_Organization_Format.acn
